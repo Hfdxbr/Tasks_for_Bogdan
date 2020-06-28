e3425617c3ae8d6f720f9f2cf1a903e4889a7c25
Ravil Shabaykin <rshabaykin@aligntech.com>
2020-06-28 18:07:51 +0300

    Add description for 03_format

---
+++ 03_format/format.h
@@ -0,0 +1,4 @@
+#include <string>
+
+template</* something */>
+std::string format(const std::string& s, /* template args */);
--- 03_format/test.cpp
+++ 03_format/test.cpp
@@ -1,1 +1,22 @@
 #include "../test_runner.h"
+#include "format.h"
+
+void Test_empty_params() {
+  ASSERT_EQUAL(format(""), "");
+  ASSERT_EQUAL(format("1"), "1");
+  ASSERT_EQUAL(format("very very long string"), "very very long string");
+}
+
+void Test_int_params() {
+  ASSERT_EQUAL(format("{} + {} = 4", 1, 3), "1 + 3 = 4");
+  ASSERT_EQUAL(format("{2} + {1} = 4", 1, 3), "3 + 1 = 4");
+  ASSERT_EQUAL(format("{1} / {2} = 0.{2}{2}{2}{2}{2}...", 1, 3), "1 / 3 = 0.33333...");
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, Test_empty_params);
+  RUN_TEST(tr, Test_int_params);
+  /* Tests */
+  return 0;
+}
